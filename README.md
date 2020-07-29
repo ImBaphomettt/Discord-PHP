2e8e3d991f50c7b7609f4ba68d32c4b9e4887934
Dylan Malandain <lucenzo.76@gmail.com>
2020-07-29 02:08:59 +0200

    Initial commit project.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DiscPHP
-Yet
+#Discord-PHP (W.I.P.E).
+
+Discord PHP is an unofficial API for interacting with the Discord voice and text service.
---
+++ src/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Dylan Malandain
+ *
+ * Class Client
+ */
+class Client
+{
+
+    /**
+     * Client constructor.
+     */
+    public function __construct()
+    {
+    }
+
+}
